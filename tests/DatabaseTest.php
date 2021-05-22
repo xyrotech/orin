790f9daf46d7808d1616d0dddb546c1f4fc31442
Adekunle Adelakun <kunle@xyrotech.com>
2021-05-22 11:39:28 -0400

    Using setup with test

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -8,15 +8,19 @@ use Xyrotech\Orin\Orin;
 
 class DatabaseTest extends TestCase
 {
+    private Orin  $discog;
+
+    public function setUp() : void
+    {
+        $config = include('configs/config.test.php');
+
+        $this->discog = new Orin($config);
+    }
 
     /** @test */
     public function verify_database_release_trait()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->release(192988);
+        $json = $this->discog->release(192988);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -25,11 +29,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_release_rating_by_user()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $release_rating = $discog->release_rating_by_user(16457562, 'kunli0');
+        $release_rating = $this->discog->release_rating_by_user(16457562, 'kunli0');
 
         // die(var_dump(json_decode($release_rating['response'])));
         $this->assertJson($release_rating['response']);
@@ -37,13 +37,13 @@ class DatabaseTest extends TestCase
 
         //Update Rating
 
-        $update_rating = $discog->update_release_rating_by_user(16457562, 'kunli0', 5);
+        $update_rating = $this->discog->update_release_rating_by_user(16457562, 'kunli0', 5);
 
         $this->assertEquals('201', $update_rating['status']);
 
         //Delete Rating
 
-        $delete_rating = $discog->delete_release_rating_by_user(16457562, 'kunli0');
+        $delete_rating = $this->discog->delete_release_rating_by_user(16457562, 'kunli0');
 
         $this->assertEquals('204', $delete_rating['status']);
     }
@@ -51,11 +51,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_community_release_rating()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->community_release_rating(16457562);
+        $json = $this->discog->community_release_rating(16457562);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -64,11 +60,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_release_stats()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->release_stats(16457562);
+        $json = $this->discog->release_stats(16457562);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -77,11 +69,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_master_release()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->master_releases(2482);
+        $json = $this->discog->master_releases(2482);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -90,11 +78,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_master_release_versions()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->master_release_versions(2482, ['sort' => 'released']);
+        $json = $this->discog->master_release_versions(2482, ['sort' => 'released']);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -103,11 +87,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_artist()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->artist(45);
+        $json = $this->discog->artist(45);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -116,11 +96,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_artist_releases()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->artist_releases(45, ['sort' => 'year']);
+        $json = $this->discog->artist_releases(45, ['sort' => 'year']);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -129,11 +105,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_label()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->label(107);
+        $json = $this->discog->label(107);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -142,11 +114,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_all_label_releases()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->all_label_releases(107);
+        $json = $this->discog->all_label_releases(107);
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
@@ -155,11 +123,7 @@ class DatabaseTest extends TestCase
     /** @test */
     public function verify_search()
     {
-        $config = include('configs/config.test.php');
-
-        $discog = new Orin($config);
-
-        $json = $discog->search('test');
+        $json = $this->discog->search('test');
 
         $this->assertJson($json['response']);
         $this->assertEquals('200', $json['status']);
