315f28c394fe5fabe74e64ea3f70a4a8f598705f
Adekunle Adelakun <kunle@xyrotech.com>
2021-07-05 12:37:17 -0400

    Added more github secrets for oauth

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -12,7 +12,22 @@ class AuthenticationTest extends TestCase
     /** @test */
     public function verify_request_token()
     {
-        $config = include('configs/config.php');
+        if (getenv('DISCOG_TOKEN')) {
+            $config = [
+                'DISCOGS_TOKEN' => getenv('DISCOG_TOKEN'),
+                'DISCOGS_CONSUMER_KEY' => getenv('DISCOG_CONSUMER_KEY'),
+                'DISCOGS_CONSUMER_SECRET' => getenv('DISCOG_CONSUMER_SECRET'),
+                'DISCOGS_VERSION' => 'v2',
+                'DISCOGS_MEDIA_TYPE' => 'discogs',
+                'DISCOGS_USER_AGENT' => 'Orin/0.1 +http://orin.xyrotech.com',
+                'OAUTH_CALLBACK' => getenv('OAUTH_CALLBACK'),
+                'RATE_THRESHOLD' => '6',
+                'USERNAME' => 'kunli0',
+            ];
+        } else {
+            $config = include('configs/config.php');
+        }
+
         $discog = new Orin($config);
 
         $oauth = $discog->request_token();
